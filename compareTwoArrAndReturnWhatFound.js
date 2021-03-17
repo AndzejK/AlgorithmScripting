f6441859afddab3de057af0c53f50f8990e951d5
MAC <andrejkrupoves@gmail.com>
2021-03-18 09:51:08 +1100

    preparation

---
+++ compareTwoArrAndReturnWhatFound.js
@@ -0,0 +1,8 @@
+/*
+Compare two arrays and return a new array with any items only found in one of the two given arrays, 
+but not both. In other words, return the symmetric difference of the two arrays.
+
+#examples
+[1,2,3,5],[1,2,3,4,5] => [4]
+*/
+
