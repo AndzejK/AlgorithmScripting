1a155911afbc71d385ee34047a118051b70235fb
MAC <andrejkrupoves@gmail.com>
2021-03-19 09:27:35 +1100

    been struggling with MAP method, still can't get my head around simple JS methods

--- compareTwoArrAndReturnWhatFound.js
+++ compareTwoArrAndReturnWhatFound.js
@@ -4,5 +4,17 @@ but not both. In other words, return the symmetric difference of the two arrays.
 
 #examples
 [1,2,3,5],[1,2,3,4,5] => [4]
+["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], 
+"diorite", "andesite", "grass", "dirt", "dead shrub"]               => ["pink wool"]
 */
 
+function diffArray(arr1,arr2){
+    let newArr=[];
+    arr1.map(x)
+    
+    return newArr
+}
+
+let arr_1 = [1,2,3,5];
+let arr_2 =  [1,2,3,4,5];
+console.log(diffArray(arr_1,arr_2));
