c9c195b8cdc1006796d2c84db427b960373d6fd4
MAC <andrejkrupoves@gmail.com>
2021-03-25 09:48:30 +1100

    Oh it took ages to solve simple task and my code long and terrible

--- compareTwoArrAndReturnWhatFound.js
+++ compareTwoArrAndReturnWhatFound.js
@@ -1,3 +1,11 @@
+
+function printDash(){
+    let dash="-";
+    for (let i=0;i<20;i++){
+        dash+="-"
+    }
+    console.log(dash);
+}
 /*
 Compare two arrays and return a new array with any items only found in one of the two given arrays, 
 but not both. In other words, return the symmetric difference of the two arrays.
@@ -10,34 +18,106 @@ but not both. In other words, return the symmetric difference of the two arrays.
 
 function diffArray(arr1,arr2){
     let newArr=[];
-   // arr1.sort((x,y)=>x-y);
-    //arr2.sort();
-    if(arr1.length>=arr2.length){
-        for(let i=0; i<arr2.length; i++){
-            if(arr2.indexOf(arr1[i])==-1){
-                newArr.push(arr1[i]);
+
+    //Check if two arrays completly different
+    if(arr1.length!=arr2.length){
+        //Find which is bigger
+        if(arr1.length>arr2.length){
+            let arrWithTheSameValues=[];
+            let arrDiffValues=[];
+            if(arr2.length==0){
+                arrDiffValues=[...arr1];
             }
+            for(let i=0;i<arr2.length;i++){
+                if(arr1.indexOf(arr2[i])==-1){
+                    arrDiffValues.push(arr2[i]);
+                }else{
+                    arrWithTheSameValues.push(arr2[i]);
+                }
+            } if(arrDiffValues.length==0){
+                for(let i=0;i<arr1.length;i++){
+                    if(arr2.indexOf(arr1[i])==-1){
+                        arrDiffValues.push(arr1[i]);
+                    }
+                } newArr=[...arrDiffValues];
+            }
+             else if(arrWithTheSameValues.length==0){
+               newArr=[...arrDiffValues,...arr1];
+            } else if(arrDiffValues.length==0){
+                for(let j=0;j<arr1.length;j++){
+                    if(arr2.indexOf(arr1[j])==-1){
+                        arrDiffValues.push(arr1[j])
+                    }
+                }
+            } newArr;
+   
+        }else{
+            let arrWithTheSameValues=[];
+            let arrDiffValues=[];
+            if(arr1.length==0){
+                arrDiffValues=[...arr2];
+            }
+            for(let i=0;i<arr1.length;i++){
+                if(arr2.indexOf(arr1[i])==-1){
+                    arrDiffValues.push(arr1[i]);
+                }else{
+                    arrWithTheSameValues.push(arr1[i]);
+                }
+            }
+            if((arrWithTheSameValues.length==0)){
+               newArr=[...arrDiffValues,...arr2];
+            }else if(arrDiffValues.length==0){
+                for(let j=0;j<arr2.length;j++){
+                    if(arr1.indexOf(arr2[j])==-1){
+                        arrDiffValues.push(arr2[j])
+                    }
+                }
+            }
+            newArr=[...arrDiffValues];
         }
-       /* for(let j=0; j<arr2.length; ++j){
-            if(arr1.indexOf(arr2[j])==-1){
-                newArr.push(arr2[j]);
-             }
-             }
-    */} else if (arr1.length<=arr2.length){
-        for(let i=0; i<arr1.length; i++){
+    } else {
+        let arrWithTheSameValues=[];
+        let arrDiffValues=[]; 
+        for(let i=0;i<arr1.length;i++){
+            if(arr2.indexOf(arr1[i])==-1){
+                arrDiffValues.push(arr1[i]);
+            }else {
+                arrWithTheSameValues.push(arr1[i])
+            }
+        } arrDiffValues;
+        for(let i=0;i<arr2.length;i++){
             if(arr1.indexOf(arr2[i])==-1){
-                newArr.push(arr2[i]);
+                arrDiffValues.push(arr2[i]);
+            }else {
+                arrWithTheSameValues.push(arr1[i])
             }
-        }
-       /* for(let i=0; i<arr2.length; i++){
-            if(arr2.indexOf(arr1[i])==-1){
-                newArr.push(arr1[i]);
-            }
-        }*/
-    } 
-    return newArr
+        } newArr=[...arrDiffValues];
+    }
+
+return newArr
 }
 
-let arr_1 = [];
-let arr_2 =  ["snuffleupagus", "cookie monster", "elmo"];
+
+//["andesite", "grass", "dirt", "dead shrub"]
+
+let arr_1 = ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"];
+let arr_2 =  ["diorite", "andesite", "grass", "dirt", "dead shrub"];
 console.log(diffArray(arr_1,arr_2));
+printDash();
+function diffArraySolutionFromFreecodecamp(arr1,arr2){
+    let newArr=[];
+    //create another function in the current function to compare two arrays
+    function findSthIn1stArr(ar1,ar2){
+        for(let i=0;i<ar1.length;i++){
+            if(ar2.indexOf(ar1[i])==-1){
+                newArr.push(ar1[i]);
+            }
+        }
+    }
+    findSthIn1stArr(arr1,arr2);
+    findSthIn1stArr(arr2,arr1);
+    return newArr;
+}
+
+console.log(diffArraySolutionFromFreecodecamp(arr_1,arr_2));
+printDash();
