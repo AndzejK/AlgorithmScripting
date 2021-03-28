758204d00a91a1b8a7beffa8cdd158dfa514e0c1
MAC <andrejkrupoves@gmail.com>
2021-03-29 10:25:30 +1100

    more complex and intresting task,however, I stuck with 2nd argument, dunno how to iterate through both arguments

--- lookingForPropertiesInArrOfObjects.js
+++ lookingForPropertiesInArrOfObjects.js
@@ -12,15 +12,20 @@ function whatIsInANname(arr,obj){
 let anArr=[];
 //FOR loop to iterate through the array
  for(let i=0;i<arr.length;i++){
-     for (const prop in arr[i]) {
-        if(arr[i][prop]==obj.last){
-            anArr.push({last:obj.last})
-        }
+     //the 2nd argument consist of object and that object can have as much as it possible properties and each of them needs to be check too
+     for (const ArrObjKey in arr[i]) {
+         for(const ObjKey in obj){
+             console.log(`ObjKey in obj(2nd arg) ${obj.ObjKey}`)
+            if(arr[i][ArrObjKey]==obj.ObjKey){
+                console.log(`${ArrObjKey}:${arr[i][ArrObjKey]}`)
+                //anArr.push({ first:arr[i].first, last:obj.last})
+            }
+         }
      }
- } return anArr
-
+ } 
+ return anArr;
 }
 
 let collection=[{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }];
-let source={last: "Capulet" };
-whatIsInANname(collection,source)
+let source={ last: "Capulet" };
+console.log(whatIsInANname(collection,source));
